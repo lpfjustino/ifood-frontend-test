552a12a9d21cc1d7525ac087d2b3cc9a8718fac4
lpfjustino <lpfjustino@gmail.com>
2019-09-18 11:31:44 -0300

    Adding tests for filter actions

--- src/javascript/actions/filters.js
+++ src/javascript/actions/filters.js
@@ -1,16 +1,14 @@
-import ROLE_SCOPE_GROUP_GET_DETAILS from "../constants";
+import { GET_TRENDING_PLAYLISTS_FILTERS } from "../constants/index.json";
 import { TRENDING_PLAYLIST_FILTERS } from "../constants/urls";
 import axios from "axios";
 
 const actionCreators = {
     getFilters() {
-        return dispatch => {
-            return dispatch({
-                type: ROLE_SCOPE_GROUP_GET_DETAILS,
-                payload: {
-                    promise: axios.get(TRENDING_PLAYLIST_FILTERS)
-                }
-            });
+        return {
+            type: GET_TRENDING_PLAYLISTS_FILTERS,
+            payload: {
+                promise: axios.get(TRENDING_PLAYLIST_FILTERS)
+            }
         };
     },
 }
