1e5383cbeef22aeb31f08d1b9425762e0c4094c1
luis <lpfjustino@gmail.com>
2019-09-22 15:51:55 -0300

    Updates

---
+++ precache-manifest.c1e2e6619ceedaad2a6b281319a93e52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5873f82864f55ef6e1fdd94d93cb8520",
+    "url": "/ifood-frontend-test/index.html"
+  },
+  {
+    "revision": "fa350270a2842b443121",
+    "url": "/ifood-frontend-test/static/css/2.b15e64a8.chunk.css"
+  },
+  {
+    "revision": "70ca7457333d0f2ca743",
+    "url": "/ifood-frontend-test/static/css/main.1a9ef300.chunk.css"
+  },
+  {
+    "revision": "fa350270a2842b443121",
+    "url": "/ifood-frontend-test/static/js/2.8f4923cd.chunk.js"
+  },
+  {
+    "revision": "70ca7457333d0f2ca743",
+    "url": "/ifood-frontend-test/static/js/main.a147ffa2.chunk.js"
+  },
+  {
+    "revision": "ed87ac6da917a6cb849d",
+    "url": "/ifood-frontend-test/static/js/runtime~main.cd2ae8d1.js"
+  }
+]);
